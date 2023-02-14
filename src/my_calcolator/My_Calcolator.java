3f87f3bd3b9df8024c0f7b7df1626add6aa5c8ad
mostafa abdelraheem <mostafaabdelraheem56@gmail.com>
2023-02-14 13:07:53 +0200

    upload project files

---
+++ src/my_calcolator/My_Calcolator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my_calcolator;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Abdelrheem
+ */
+public class My_Calcolator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+            frmCalc f = new frmCalc();
+            f.setVisible(true);
+            f.setLocationRelativeTo(null);
+            
+//            
+    }
+    
+}
